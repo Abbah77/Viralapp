ad624d4ece81d9fac6450ceaf49520ca0d3b02e8
Abbah77 <yakubuyakson77@gmail.com>
2026-04-20 21:26:23 +0100

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 import '../models/video_model.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://tt-b577.onrender.com';
+  static const String _base = 'https://tt-b577.onrender.com';
 
   static final Dio _dio = Dio(BaseOptions(
-    baseUrl: baseUrl,
+    baseUrl: _base,
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 10),
     headers: {
@@ -15,11 +15,7 @@ class ApiService {
   ));
 
   static Future<FeedResponse> getFeed({int page = 1}) async {
-    try {
-      final res = await _dio.get('/feed', queryParameters: {'page': page});
-      return FeedResponse.fromJson(res.data);
-    } catch (e) {
-      throw Exception('Failed to load feed: $e');
-    }
+    final res = await _dio.get('/feed', queryParameters: {'page': page});
+    return FeedResponse.fromJson(res.data as Map<String, dynamic>);
   }
 }
