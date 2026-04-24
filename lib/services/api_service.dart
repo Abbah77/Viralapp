02bd4c36e8f2c5b74dbcd8e931c952f84992e49a
Abbah77 <yakubuyakson77@gmail.com>
2026-04-24 21:40:05 +0100

    Refactor ApiService with search method and updates
    Updated import statement and modified getFeed method to remove type casting. Added search method to retrieve videos based on a query.

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import '../models/video_model.dart';
+import '../models/models.dart';
 
 class ApiService {
   static const String _base = 'https://tt-b577.onrender.com';
@@ -8,14 +8,22 @@ class ApiService {
     baseUrl: _base,
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 10),
-    headers: {
-      'Accept-Encoding': 'gzip',
-      'Content-Type': 'application/json',
-    },
+    headers: {'Accept-Encoding': 'gzip'},
   ));
 
   static Future<FeedResponse> getFeed({int page = 1}) async {
     final res = await _dio.get('/feed', queryParameters: {'page': page});
-    return FeedResponse.fromJson(res.data as Map<String, dynamic>);
+    return FeedResponse.fromJson(res.data);
+  }
+
+  static Future<List<VideoModel>> search(String query) async {
+    try {
+      final res = await _dio.get('/search', queryParameters: {'q': query});
+      return (res.data['videos'] as List? ?? [])
+          .map((v) => VideoModel.fromJson(v))
+          .toList();
+    } catch (_) {
+      return [];
+    }
   }
 }
